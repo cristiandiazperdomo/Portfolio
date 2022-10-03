41c0e3838badeed634646015738547d4206f2bc4
cristiandiazperdomo <danubiodiaz2003edf@gmail.com>
2022-10-03 10:24:38 -0300

    new decoration, waves and size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,45 +4,47 @@ import './Header.css';
 export const Header = () => {
 	return (
 		<header className="Header">
-			<div className="left-side">
-				<Link to="/">
-					<p className="Header-title">
-						<span>C</span>
-						D
-					</p>
-				</Link>
-				<Link to="/">
-					<p>
-						Portfolio
-					</p>
-				</Link>
+			<div className="Header-div">	
+				<div className="left-side">
+					<Link to="/">
+						<p className="Header-title">
+							<span>C</span>
+							D
+						</p>
+					</Link>
+					<Link to="/">
+						<p>
+							Portfolio
+						</p>
+					</Link>
+				</div>
+				<nav>
+					<div className="right-side">	
+						<nav>
+							<ul className="Header-ul">
+								<li>
+									<a href="https://github.com/cristiandiazperdomo">
+										Otros proyectos
+									</a> 
+								</li>
+								<li>
+									<a href="https://www.linkedin.com/in/cristian-d%C3%ADaz-390971222/">
+										Linkedin
+									</a>
+								</li>
+								<li>
+									<a href="https://github.com/cristiandiazperdomo">
+										GitHub
+									</a>
+								</li>
+							</ul>
+						</nav>
+					</div>
+					<div className="hamburger-menu-container">
+						<img src={require('../../img/menu.png')} alt="hamburger-menu"/>
+					</div>
+				</nav>
 			</div>
-			<nav>
-				<div className="right-side">	
-					<nav>
-						<ul className="Header-ul">
-							<li>
-								<a href="https://github.com/cristiandiazperdomo">	
-									Otros proyectos
-								</a> 
-							</li>
-							<li>
-								<a href="https://www.linkedin.com/in/cristian-d%C3%ADaz-390971222/">
-									Linkedin
-								</a>
-							</li>
-							<li>
-								<a href="https://github.com/cristiandiazperdomo">
-									GitHub
-								</a>
-							</li>
-						</ul>
-					</nav>
-				</div>
-				<div className="hamburger-menu-container">
-					<img src={require('../../img/menu.png')} alt="hamburger-menu"/>
-				</div>
-			</nav>
 		</header>
 	)
 } 
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -42,6 +42,12 @@ export const Home = () => {
 						<h1 className="presentation-title">Cristian Díaz</h1>
 						<p className="rol">Front-End Developer</p>
 					</article>
+					<footer>
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+							<path fill="#019267" fillOpacity="1" d="M0,160L60,176C120,192,240,224,360,234.7C480,245,600,235,720,208C840,181,960,139,1080,122.7C1200,107,1320,117,1380,122.7L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z">
+							</path>
+						</svg>
+					</footer>
 				</div>
 				<div className="whoiam">
 					<div className="whoiam-two">
